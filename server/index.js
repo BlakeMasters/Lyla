741d472fe0135436dcfe5268aea250869cedc37d
blake <blakemasters31@gmail.com>
2025-01-28 14:30:46 -0800

    .env removed?

--- server/index.js
+++ server/index.js
@@ -2,12 +2,20 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err))
 
 dotenv.config()
 const port = process.env.PORT
@@ -18,8 +26,10 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Express Backend is running!')
+  res.send('Express + MongoDB + JWT API running')
 })
+
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.get('/api/greeting', (req, res) => {
   res.json({ message: 'Hello from the Express API!' })
